6fa2a87a31dc84fa0640f5aeb9ba566e59d04501
panz2018 <49798383+panz2018@users.noreply.github.com>
2025-02-10 21:45:52 -0500

    Display equations in vitepress

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   title: 'RF Network',
   description:
     'Reading/writing touchstone snp files, similar to SignalIntegrity and scikit-rf in python',
+
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
@@ -18,5 +19,10 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/panz2018/Network-rf.ts' },
     ],
+  },
+
+  // Show LaTex equations in the markdown
+  markdown: {
+    math: true,
   },
 })
--- src/touchstone.ts
+++ src/touchstone.ts
@@ -1,17 +1,40 @@
 export type SParameterFormat = 'MA' | 'DB' | 'RI' | undefined
 
 /**
- * Class to read touchstone s-parameter files.<br>
- * When \\(a \ne 0\\), there are two solutions to \(ax^2 + bx + c = 0\)
- * and they are $$x = {-b \pm \sqrt{b^2-4ac} \over 2a}.$$
- * <br>
+ * Class to read touchstone s-parameter files.
+ * When $a \ne 0$, there are two solutions to $ax^2 + bx + c = 0$
+ * and they are
+ * $$
+ * x = {-b \pm \sqrt{b^2-4ac} \over 2a}
+ * $$
+ * 
+ * $E=mc^2$
+ * 
+ * 这是一个行内公式：$E=mc^2$ 。
+
+这是一个块级公式：
+$$
+\int_{a}^{b} f(x) \,dx
+$$
+
  * 公式为 $A = \pi r^2$，其中 $r$ 是半径
  * 这是一个独立公式：
  * $$
  * d = \sqrt{(x_2 - x_1)^2 + (y_2 - y_1)^2}
  * $$
- * When \\(a \ne 0\\), there are two solutions to \\(ax^2 + bx + c = 0\\)
- * and they are \\[x = {-b \pm \sqrt{b^2-4ac} \over 2a}.\\]
+ * When \(a \ne 0\), there are two solutions to \(ax^2 + bx + c = 0\)
+ * and they are \[x = {-b \pm \sqrt{b^2-4ac} \over 2a}\]
+ * 
+ * When $a \ne 0$, there are two solutions to $(ax^2 + bx + c = 0)$ and they are
+$$ x = {-b \pm \sqrt{b^2-4ac} \over 2a} $$
+
+**Maxwell's equations:**
+
+| equation                                                                                                                                                                  | description                                                                            |
+| ------------------------------------------------------------------------------------------------------------------------------------------------------------------------- | -------------------------------------------------------------------------------------- |
+| $\nabla \cdot \vec{\mathbf{B}}  = 0$                                                                                                                                      | divergence of $\vec{\mathbf{B}}$ is zero                                               |
+| $\nabla \times \vec{\mathbf{E}}\, +\, \frac1c\, \frac{\partial\vec{\mathbf{B}}}{\partial t}  = \vec{\mathbf{0}}$                                                          | curl of $\vec{\mathbf{E}}$ is proportional to the rate of change of $\vec{\mathbf{B}}$ |
+| $\nabla \times \vec{\mathbf{B}} -\, \frac1c\, \frac{\partial\vec{\mathbf{E}}}{\partial t} = \frac{4\pi}{c}\vec{\mathbf{j}}    \nabla \cdot \vec{\mathbf{E}} = 4 \pi \rho$ | _wha?_                                                                                 |
  *
  * # References
  * - {@link https://ibis.org/touchstone_ver2.1/touchstone_ver2_1.pdf Touchstone(R) File Format Specification (Version 2.1)}
